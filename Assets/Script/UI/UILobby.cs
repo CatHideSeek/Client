089ecfd1f8d7bc0ad0bcd1e78ea503b0a8d1c304
kyn320 <kyn320@naver.com>
2017-06-08 16:16:59 +0900

    RoomSlot, UI Animation

--- Assets/Script/UI/UILobby.cs
+++ Assets/Script/UI/UILobby.cs
@@ -6,17 +6,108 @@ using UnityEngine.UI;
 /// <summary>
 /// 로비 UI 클래스입니다.
 /// </summary>
-public class UILobby : MonoBehaviour {
+public class UILobby : MonoBehaviour
+{
 
-    
+    public static UILobby instance;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Animator menuAni;
+
+    public Text roomListText;
+    public Animator rommListAni;
+
+    public GameObject roomSlot;
+    public List<GameObject> roomSlotList;
+
+    public Vector2 startPos, marginPos;
+
+    public RectTransform roomScrollView;
+
+    bool isOpenRoomList = false, isOpenMenu = false;
+
+    public Text[] chatLog;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void CreateRoomList()
+    {
+        //NetworkManager.instance.roomList;
+        //roomScrollView
+
+        roomScrollView.sizeDelta = new Vector2(0, NetworkManager.instance.roomList.Count * -marginPos.y + 10f);
+
+        for (int i = 0; i < NetworkManager.instance.roomList.Count; i++)
+        {
+            GameObject g = Instantiate(roomSlot);
+            roomSlotList.Add(g);
+            g.GetComponent<UIRoomSlot>().SetData(i + 1, NetworkManager.instance.roomList[i]);
+            RectTransform tr = g.GetComponent<RectTransform>();
+            tr.parent = roomScrollView;
+            tr.localPosition = startPos + new Vector2(marginPos.x, marginPos.y * i);
+            tr.localScale = new Vector2(1, 1);
+        }
+
+    }
+
+    void RemoveRoomList()
+    {
+        if (roomSlotList.Count < 1)
+            return;
+
+        for (int i = 0; i < roomSlotList.Count; i++)
+        {
+            Destroy(roomSlotList[i]);
+        }
+    }
+
+    public void OnMenu()
+    {
+        isOpenMenu = !isOpenMenu;
+        if (isOpenMenu)
+            menuAni.SetTrigger("Open");
+        else
+            menuAni.SetTrigger("Close");
+    }
+
+    public void OnroomList()
+    {
+        isOpenRoomList = !isOpenRoomList;
+        if (isOpenRoomList)
+        {
+            RemoveRoomList();
+            rommListAni.SetTrigger("Open");
+            CreateRoomList();
+            roomListText.text = "Close";
+        }
+        else
+        {
+            rommListAni.SetTrigger("Close");
+            roomListText.text = "Open";
+        }
+    }
+
+    public void SendChat(InputField input)
+    {
+        //NetworkManager.instance.SendChat(PlayerDataManager.instance.my.name, input.text, 0);
+        for (int i = 0; i < chatLog.Length; i++)
+        {
+            chatLog[i].text += "User : " + input.text + "\n";
+            chatLog[i].transform.parent.parent.GetComponent<ScrollRect>().verticalNormalizedPosition = 0f;
+        }
+
+        input.text = "";
+    }
+
+    public void AddChatLog(string message)
+    {
+        for (int i = 0; i < chatLog.Length; i++)
+        {
+            chatLog[i].text += message + "\n";
+        }
+    }
+
+
 }
---
+++ Assets/Script/UI/UIRoomSlot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRoomSlot : MonoBehaviour {
+
+    public Text numberText, nameText, countText, stateText;
+
+    public void SetData(int index,Room room) {
+        numberText.text = "No. " + index;
+        nameText.text = room.name;
+        countText.text = room.countPlayers + " / " + room.maxPlayers;
+        if (room.isPlay)
+            stateText.text = "<color=#ff0000ff>Playing..</color>";
+        else if (room.countPlayers == room.maxPlayers)
+            stateText.text = "<color=#00ffffff>Full..</color>";
+        else
+            stateText.text = "<color=#00ff00ff>Wait..</color>";
+    }
+
+}
